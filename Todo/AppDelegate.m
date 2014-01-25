c0325db3599f153c43386f2d4f5a897f98d94253
Adam Tait <adam@adamtait.com>
2014-01-24 21:45:31 -0800

    create new project. Add custom TableViewController (TodoViewController) & TableViewCell (EditableCell). Connect TodoViewController to application using NavigationController.initWithRootViewController

--- Todo/AppDelegate.m
+++ Todo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TodoViewController.h"
 
 @implementation AppDelegate
 
@@ -16,6 +17,13 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    
+    TodoViewController *todoViewController = [[TodoViewController alloc] init];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:todoViewController];
+    
+    self.window.rootViewController = navController;
+    
     return YES;
 }
 
---
+++ Todo/EditableCell.h
@@ -0,0 +1,13 @@
+//
+//  EditableCell.h
+//  Todo
+//
+//  Created by Adam Tait on 1/24/14.
+//  Copyright (c) 2014 Adam Tait. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditableCell : UITableViewCell
+
+@end
